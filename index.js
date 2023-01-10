2615266f503a50702d23e7ae03ca3d86a87c0370
sajid365-sr <sajidsorker86@gmail.com>
2023-01-10 14:14:08 +0600

    user connect or disconnect status setup

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const http = require('http');
 const app = express();
+const { Server } = require('socket.io');
+
+const httpServer = http.createServer(app);
+const io = new Server(httpServer);
 
 
-const server = http.createServer(app);
-
+io.on("connection", (socket) => {
+    console.log("new user connected to our application");
+    socket.on("disconnect", (socket) => {
+        console.log("user disconnected from our application");
+    })
+})
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/app.html');
 })
 
-app.listen(5000, function(){
+httpServer.listen(5000, function () {
     console.log('listening on port 5000');
 });
