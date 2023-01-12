af37df3f591027b9bf750ab694552bbe362e392f
sajid365-sr <sajidsorker86@gmail.com>
2023-01-12 19:32:17 +0600

    Broadcasting event practice

--- index.js
+++ index.js
@@ -7,12 +7,30 @@ const httpServer = http.createServer(app);
 const io = new Server(httpServer);
 
 
-io.on("connection", (socket) => {
-    console.log("new user connected to our application");
-    socket.on("disconnect", (socket) => {
-        console.log("user disconnected from our application");
-    })
+// io.on("connection", (socket) => {
+//     console.log("new user connected to our application");
+//     socket.on("disconnect", (socket) => {
+//         console.log("user disconnected from our application");
+//     })
+
+//     // socket.send("hello world");
+//     socket.on("testEvent", (message) => {
+//         console.log("user sent a message: ", message);
+//     });
+
+//     // Broadcasting event
+//     io.sockets.emit("FIFA world cup", "Hello football")
+// })
+
+
+let fifa = io.of("/worldCup");
+
+fifa.on("connection", (socket) => {
+    fifa.emit("worldCupEvent", "Hello fifa world cup");
 })
+
+
+
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/app.html');
